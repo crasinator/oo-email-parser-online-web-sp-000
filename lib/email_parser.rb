5001aabe0a9752989f0e0f824d947793cf3f3ed3
Jordan Reed <jordanreed03@gmail.com>
2021-01-05 05:28:21 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -16,7 +16,7 @@ class EmailAddressParser
     
     @@all.each do |email|
       arr = [ ]
-      arr.push(email.split(", "))
+      arr.push(email.split(", ").split)
       end 
     return arr.flatten
   end 
